a7006c71b4ad27ddaedd9303f0e55c701871cd16
natalia-bialek <natalie.protein@gmail.com>
2023-07-22 23:42:28 +0200

    Clean HabitList component

--- front/src/components/HabitList/HabitList.js
+++ front/src/components/HabitList/HabitList.js
@@ -1,111 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "./HabitList.module.css";
-import Habit from "../Habit/Habit";
-import NewHabit from "../NewHabit/NewHabit";
-import EditHabit from "../EditHabit/EditHabit";
-import axios from "../../axios.js";
-import { useHabitStore } from "../../store";
 import HabitSummary from "../HabitSummary/HabitSummary";
 import { useFetchHabits } from "../../hooks/useFetchHabits.js";
-import { useHabit } from "../../hooks/useHabit";
 
-function HabitList(props) {
+// async function addHabit(object) {
+//   //backend
+//   try {
+//     const result = await axios.post("/habits", object);
+//     const newHabit = result.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   //frontend
+// }
+
+function HabitList() {
   const habits = useFetchHabits();
-  const addHabit = useHabitStore((state) => state.addHabit);
-  const editHabit = useHabitStore((state) => state.editHabit);
-  const deleteHabit = useHabitStore((state) => state.deleteHabit);
-
-  //show NewHabit component
-  const [isNewOpen, setIsNewOpen] = useState(false);
-
-  //modal to edit data
-  const [isEditOpen, setIsEditOpen] = useState(false);
-
-  //show all info about habit
-  const [isHabitOpen, setIsHabitOpen] = useState(false);
-
-  //which habit should be edited
-  const [editingHabit, setEditingHabit] = useState({});
-
-  async function addNewHabit(habit) {
-    try {
-      addHabit(habit);
-      toggleComponent();
-    } catch (error) {
-      console.error(error);
-      //NotificationManager.error(error.response.data.message);
-    }
-  }
-
-  async function editNewHabit(habit) {
-    try {
-      editHabit(habit);
-      toggleEdit();
-    } catch (error) {
-      //NotificationManager.error(error.response.data.message);
-    }
-  }
-
-  function toggleEdit() {
-    setIsEditOpen(!isEditOpen);
-  }
-
-  function toggleComponent() {
-    setIsNewOpen(!isNewOpen);
-  }
-
-  function editHabitHandler(habit) {
-    toggleEdit();
-  }
-
-  function showHabitHandler(habit) {
-    toggleShow();
-    setEditingHabit(habit);
-  }
-
-  function toggleShow() {
-    setIsHabitOpen(!isHabitOpen);
-  }
-
-  async function showHabit(habit) {
-    // try {
-    //   //backend
-    //   await axios.put("/habits/" + habit._id, habit);
-    //   //frontend
-    //   const h = [...habits];
-    //   const index = h.findIndex((item) => item._id === habit._id);
-    //   if (index >= 0) {
-    //     h[index] = habit;
-    //     setHabits(h);
-    //   }
-    //   toggleShow();
-    // } catch (error) {
-    //   NotificationManager.error(error.response.data.message);
-    // }
-  }
-
-  // useEffect(() => {
-  //   // loadHabits();
-  // }, [habits]);
 
   return (
-    <>
-      <button id="newHabitButton" onClick={toggleComponent}>
-        Dodaj nawyk
-      </button>
-      {isNewOpen && (
-        <NewHabit
-          onCancel={() => setIsNewOpen(false)}
-          onAdd={(habit) => addNewHabit(habit)}
-        />
-      )}
-
-      <div className={styles.habitList}>
-        {habits.map((object, key) => (
-          <HabitSummary key={key} object={object} />
-        ))}
-      </div>
-    </>
+    <div className={styles.habitList}>
+      {habits.map((object, key) => (
+        <HabitSummary key={key} object={object} />
+      ))}
+    </div>
   );
 }
 
